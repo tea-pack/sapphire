5cc53f92998bd452a2607934b766db200e2f1b52
BcemXaxa <artemderk@mail.ru>
2024-11-28 15:44:08 +0300

    added client parser and csvparser

--- src/main/java/com/github/tea_pack/sapphire/parsers/ClientParser.java
+++ src/main/java/com/github/tea_pack/sapphire/parsers/ClientParser.java
@@ -2,6 +2,7 @@ package com.github.tea_pack.sapphire.parsers;
 
 import com.github.tea_pack.sapphire.entities.Client;
 import com.github.tea_pack.sapphire.entities.Gender;
+import com.github.tea_pack.sapphire.entities.Range;
 
 import java.util.Locale;
 
@@ -10,6 +11,12 @@ public class ClientParser {
 		String[] split = line.split(";");
 		int ID = Integer.parseInt(split[0].substring(1));
 		String address = split[1];
-		Gender gender = switch (split[2].toLowerCase(Locale.))
+		Gender gender = switch (split[2].toUpperCase(Locale.of("ru"))){
+			case "М" -> Gender.MALE;
+			case "Ж" -> Gender.FEMALE;
+			default -> Gender.UNKNOWN;
+		};
+		Range age = Range.from(split[3]);
+		return new Client(ID, address, gender, age);
 	}
 }
