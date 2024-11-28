22171c84dfc1536035b6211868ff5677dfc46c43
IA1phaI <pavel.2014.play@gmail.com>
2024-11-28 12:57:41 +0300

    chore: mv package into app

---
+++ app/src/main/java/com/github/tea_pack/sapphire/SapphireApplication.java
@@ -0,0 +1,14 @@
+package com.github.tea_pack.sapphire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SapphireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapphireApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/github/tea_pack/sapphire/SapphireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tea_pack.sapphire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapphireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
