2c43189e3e81a4347e51b29ff032c1b001576af1
IA1phaI <pavel.2014.play@gmail.com>
2024-11-28 21:24:09 +0300

    refact: architecture changes in build conf + gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework:spring-web:6.2.0")
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
