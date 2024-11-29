fc1b82c6453c148ead0222ede0caa871dfff80c9
BcemXaxa <artemderk@mail.ru>
2024-11-29 13:21:06 +0300

    todo: fix

--- src/main/java/com/github/tea_pack/sapphire/statistics/ChannelPopulartiy.java
+++ src/main/java/com/github/tea_pack/sapphire/statistics/ChannelPopulartiy.java
@@ -1,11 +1,18 @@
 package com.github.tea_pack.sapphire.statistics;
 
+import com.github.tea_pack.sapphire.entities.Broadcast;
+import com.github.tea_pack.sapphire.entities.View;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class ChannelPopulartiy {
-    // public class Stats {
-    // int totalTimeWatched;
-    // int totalWatchers;
-    // }
-    // public HashMap<Integer, Stats> getBroadcastWatchTime() {
-    //
-    // }
+     public class Stats {
+     	int totalTimeWatched;
+     	int totalWatchers;
+     }
+     public HashMap<Broadcast, Stats> getBroadcastWatchTime(List<View> views) {
+		 HashMap<Broadcast, Stats> map = new HashMap<>();
+
+     }
 }
